56961a320c3ecc1edd0460b5c905ca2fd63e9a07
Louis <louisgerardoserranoalamo@gmail.com>
2024-04-24 12:35:04 -0600

    Se agrego un metodo para procesar imagenes CtkImage

--- util/ProcesarImagenes.py
+++ util/ProcesarImagenes.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageTk
-
+from customtkinter import CTkImage
 def rescale_image(image_path, new_width, new_height):
     img = Image.open(image_path)
     img_rescaled = img.resize((new_width, new_height))
@@ -8,4 +8,13 @@ def rescale_image(image_path, new_width, new_height):
     tk_image = ImageTk.PhotoImage(img_rescaled)
 
     return tk_image
+
+def rescale_image_ctk(image_path, new_width, new_height):
+    img = Image.open(image_path)
+    img_rescaled = img.resize((new_width, new_height))
+
+    # Convertir la imagen PIL a una imagen CTkImage
+    ctk_image = CTkImage(img_rescaled)
+
+    return ctk_image
 
