34c04899aae2a2dd63051b0e63e952c5362c5892
Louis <louisgerardoserranoalamo@gmail.com>
2024-04-25 08:18:00 -0600

    ...

--- front_end/componentes_graficos/individuales/LtkButton/LtkButtonImage.py
+++ front_end/componentes_graficos/individuales/LtkButton/LtkButtonImage.py
@@ -19,14 +19,6 @@ class LtkButtonImage(CTkButton):
             command=funcion
         )
 
-import tkinter as tk
-from util.ProcesarImagenes import rescale_image_ctk
-ventana = tk.Tk()
-from PIL import Image, ImageTk
-imagene = rescale_image_ctk(image_path='../../../../imagenes/iconos/filtrar.png', new_width=30, new_height=30)
-boton = LtkButtonImage(ventana, image=imagene, funcion=lambda: print("hola"))
-boton.pack()
-ventana.mainloop()
 
 
 
